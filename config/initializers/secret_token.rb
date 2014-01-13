9da8759fd184001aedcae3c606212a1c1f25dffb
sharifzubi <szubi@me.com>
2014-01-13 13:23:29 +0000

    first commit with README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thezubiway::Application.config.secret_token = '7d2942e195a35f9adbc72eaf8488662a7b5fd34c1e1c42c26a44f51a229416ff4e3591c8db2bf47931b0880c1bf5c21353bad156594ff9fde757ceb71053633b'
